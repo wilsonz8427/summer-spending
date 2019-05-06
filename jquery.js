30867956de6b34e56b35d95b611c44afb3797df3
Wilson Zhang <wilsonz8427@hstat.org>
2019-05-06 00:57:12 +0000

    change layout of info diplay with buttons

--- jquery.js
+++ jquery.js
@@ -1,3 +1,30 @@
 $(function() {
-    $('body').fadeIn(2000)
+    $('body').fadeIn(1500);
+    
+    
+    $('#hikebut').click(function(){
+        $('#hikeinfo').toggleClass('hidden')
+    });
+    
+    $('#beachbut').click(function(){
+        $('#beachinfo').toggleClass('hidden')
+    });
+    
+    $('#amusebut').click(function(){
+        $('#amuseinfo').toggleClass('hidden')
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 });
