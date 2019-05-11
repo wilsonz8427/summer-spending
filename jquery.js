ad915de475b947028a0e531e9d6df7d8f1297568
Bairu Li <bairul6154@hstat.org>
2019-05-11 18:40:04 +0000

    add page transition

--- jquery.js
+++ jquery.js
@@ -24,10 +24,27 @@ $(function() {
     });
 
 
-    $('#left-arrow').click(function() {
-        
-    });
 
+    $("a").on("click", function(event){
+        event.preventDefault(); //stops the a automatic change
+        
+        const href = $(this).attr("href"); //gets the href attribute from the clicked a
+        
+        window.history.pushState(null,null,href); //changes the url of page without refresh
+        
+        $.ajax({ //tbh no idea
+            url: href,
+            success: function(data) { 
+                $("section").fadeOut(500, function(){
+                    
+                    const newPage = $(data).filter("section").html(); //gets data from the clicked a href 
+                    $("section").html(newPage); //sets the data to the current section
+                    
+                    $("section").fadeIn(500);
+                });
+            }
+        });
+    });
 
 
 
