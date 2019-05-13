7fbf45689dfdad165961eca6921e97ee6171106b
Bairu Li <bairul6154@hstat.org>
2019-05-13 00:59:00 +0000

    fix buttons

--- jquery.js
+++ jquery.js
@@ -2,17 +2,7 @@ $(function() {
     $('body').fadeIn(1500);
 
 
-    $('#cook').click(function() {
-        $('#cookp').toggleClass('hidden');
-    });
-
-    $('#game').click(function() {
-        $('#gamep').toggleClass('hidden');
-    });
-
-    $('#party').click(function() {
-        $('#partyp').toggleClass('hidden');
-    });
+    
 
 
     $('#hikebut').click(function() {
@@ -25,25 +15,35 @@ $(function() {
 
 
 
+    var allowSubmit = true;
     $("a").on("click", function(event) {
-        event.preventDefault(); //stops the a automatic change
+        allowSubmit = true;
+console.log(allowSubmit)
+        if (allowSubmit == true) {
+            event.preventDefault(); //stops the a automatic change
 
-        const href = $(this).attr("href"); //gets the href attribute from the clicked a
+            const href = $(this).attr("href"); //gets the href attribute from the clicked a
 
-        window.history.pushState(null, null, href); //changes the url of page without refresh
+            window.history.pushState(null, null, href); //changes the url of page without refresh
 
-        $.ajax({ //tbh no idea
-            url: href,
-            success: function(data) {
-                $("section").fadeOut(500, function() {
+            $.ajax({ //tbh no idea
+                url: href,
+                success: function(data) {
+                    $("section").fadeOut(500, function() {
 
-                    const newPage = $(data).filter("section").html(); //gets data from the clicked a href 
-                    $("section").html(newPage); //sets the data to the current section
+                        const newPage = $(data).filter("section").html(); //gets data from the clicked a href 
+                        $("section").html(newPage); //sets the data to the current section
 
-                    $("section").fadeIn(500);
-                });
-            }
-        });
+                        $("section").fadeIn(500);
+                        
+                    });
+                    
+                }
+                
+            });
+            allowSubmit = false
+        }
+console.log(allowSubmit)
     });
 
 
